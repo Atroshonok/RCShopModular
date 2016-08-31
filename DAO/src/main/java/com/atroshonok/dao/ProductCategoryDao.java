d2f6c649d80d48884a3f5a7348b578eaa55e204d
Wlancer <atroshonok@tut.by>
2016-08-31 19:21:16 +0300

    added ProductCategoryDao and ProductCategoryService

---
+++ DAO/src/main/java/com/atroshonok/dao/ProductCategoryDao.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.atroshonok.dao;
+
+import com.atroshonok.dao.entities.ProductCategory;
+
+/**
+ * @author Ivan Atroshonok
+ *
+ */
+public class ProductCategoryDao extends BaseDao<ProductCategory> {
+
+}
